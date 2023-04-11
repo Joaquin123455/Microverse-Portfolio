7889bc7cc46b75d316a626ff217390cc2251e237
Joaquin Calle Aranzolo <joaquincalleunlp@hotmail.com>
2023-04-11 18:05:48 -0300

    Add linters and menu.js

---
+++ menu.js
@@ -0,0 +1,17 @@
+const btncls = document.querySelector('#closeOnClick');
+const btnopn = document.querySelector('#openOnClick');
+const menuItem = document.querySelectorAll('#menuOpt .li-nav-bar-hidden .a-nav-bar-hidden');
+
+const btnOpnNav = () => {
+  document.getElementById('myNav').style.height = '100%';
+};
+
+const btncloseNav = () => {
+  document.getElementById('myNav').style.height = '0%';
+};
+
+btnopn.addEventListener('click', btnOpnNav);
+btncls.addEventListener('click', btncloseNav);
+menuItem.forEach((item) => {
+  item.addEventListener('click', btncloseNav);
+});
