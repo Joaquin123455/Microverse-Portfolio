ee6efe3e9d9e02ca712c328d9c85c2cf2bc51e7d
Joaquin Calle Aranzolo <joaquincalleunlp@hotmail.com>
2023-04-18 16:45:35 -0300

    Fix Required Changes

--- js/popup-window.js
+++ js/popup-window.js
@@ -50,166 +50,108 @@ const cardetails = [
 ];
 
 const cardWorks = document.querySelector('#portfolio');
-const cardWorks1 = document.querySelector('#portfolio1');
-const cardWorks2 = document.querySelector('#portfolio2');
-const cardWorks3 = document.querySelector('#portfolio3');
 
 cardWorks.className = 'works';
-const workCard = document.createElement('article');
-const workCardDiv = document.createElement('div');
-workCardDiv.className = 'card-works';
-cardWorks.appendChild(workCard);
-workCard.className = 'card-works';
-workCard.innerHTML = `<img src= ${cardetails[0].img} alt="snapshootimg" class="snapshoot-img">
-<img src= ${cardetails[0].imagedsk} alt="snapshootdsk" class="snapshoot-img-dsk">
-<div class="adjust">
-  <div class="leftblock">
-    <h2 class="card-works-title popup-title">${cardetails[0].title}</h2>
-    <div class="ref">
-      <span class="client">${cardetails[0].spans[0]}</span>
-      <span><i class="fa fa-circle fa-xs"></i></span>
-      <span>${cardetails[0].spans[1]}</span>
-      <span><i class="fa fa-circle"></i></span>
-      <span class="year">${cardetails[0].spans[2]}</span>
+for (let i = 0; i < cardetails.length; i++) {
+  const workCard = document.createElement('article');
+  const workCardDiv = document.createElement('div');
+  workCardDiv.className = 'card-works';
+  cardWorks.appendChild(workCard);
+  workCard.className = 'card-works';
+  workCard.innerHTML = `<img src= ${cardetails[i].img} alt="snapshootimg" class="snapshoot-img">
+  <img src= ${cardetails[i].imagedsk} alt="snapshootdsk" class="snapshoot-img-dsk">
+  <div class="adjust">
+    <div class="leftblock">
+      <h2 class="card-works-title popup-title">${cardetails[i].title}</h2>
+      <div class="ref">
+        <span class="client">${cardetails[i].spans[0]}</span>
+        <span><i class="fa fa-circle fa-xs"></i></span>
+        <span>${cardetails[i].spans[1]}</span>
+        <span><i class="fa fa-circle"></i></span>
+        <span class="year">${cardetails[i].spans[2]}</span>
+      </div>
+      <p class="card-primary-text">${cardetails[i].textcontent}</p>
     </div>
-    <p class="card-primary-text">${cardetails[0].textcontent}</p>
-  </div>
-  <ul class="tags">
-    <li class="tag">${cardetails[0].technologies[0]}</li>
-    <li class="tag">${cardetails[0].technologies[1]}</li>
-    <li class="tag">${cardetails[0].technologies[2]}</li>
-  </ul>
-  <div class="ref">
-    <button class="popup-btn" id="1">${cardetails[0].btntxt}</button>
-  </div>
-</div>`;
-
-cardWorks1.className = 'works-2';
-const workCard1 = document.createElement('article');
-const workCardDiv1 = document.createElement('div');
-workCardDiv1.className = 'card-works';
-cardWorks1.appendChild(workCard1);
-workCard1.className = 'card-works';
-workCard1.innerHTML = `<img src=${cardetails[1].img} alt="snapshootimg" class="snapshoot-img">
-<div class="adjust">
-  <div class="leftblock">
-    <h2 class="card-works-title-2 popup-title">${cardetails[1].title}</h2>
+    <ul class="tags">
+      <li class="tag">${cardetails[i].technologies[0]}</li>
+      <li class="tag">${cardetails[i].technologies[1]}</li>
+      <li class="tag">${cardetails[i].technologies[2]}</li>
+    </ul>
     <div class="ref">
-      <span class="client-1">${cardetails[1].spans[0]}</span>
-      <span class="client-hidden">${cardetails[1].spansdsk[0]}</span>
-      <span><i class="fa fa-circle fa-xs"></i></span>
-      <span class="role">${cardetails[1].spans[1]}</span>
-      <span><i class="fa fa-circle"></i></span>
-      <span class="year">${cardetails[1].spans[2]}</span>
+      <button class="popup-btn" id="1">${cardetails[i].btntxt}</button>
     </div>
-    <p class="card-primary-text">${cardetails[1].textcontent}</p>
-  </div>
-  <ul class="tags">
-    <li class="tag">${cardetails[1].technologies[0]}</li>
-    <li class="tag">${cardetails[1].technologies[1]}</li>
-    <li class="tag-hidden">${cardetails[1].technologiesdsk[2]}</li>
-    <li class="tag">${cardetails[1].technologies[2]}</li>
-  </ul>
-  <div class="ref">
-    <button class="popup-btn" id= "2">${cardetails[1].btntxt}</button>
-  </div>
-</div>
-<img src=${cardetails[1].imagedsk} alt="snapshootdsk" class="snapshoot-img-dsk-2">`;
+  </div>`;
+  cardWorks.appendChild(workCard);
+}
 
-cardWorks2.className = 'works-3';
-const workCard2 = document.createElement('article');
-const workCardDiv2 = document.createElement('div');
-workCardDiv2.className = 'card-works';
-cardWorks2.appendChild(workCard2);
-workCard2.className = 'card-works';
-workCard2.innerHTML = `<img src=${cardetails[2].img} alt="snapshootimg" class="snapshoot-img">
-<img src=${cardetails[2].imagedsk} alt="snapshootdsk" class="snapshoot-img-dsk">
-<div class="adjust">
-  <div class="leftblock">
-    <h2 class="card-works-title-1 popup-title">${cardetails[2].titledsk}</h2>
-    <h2 class="card-works-title-hidden">${cardetails[2].titledsk}</h2>
-    <div class="ref">
-      <span class="client-1">${cardetails[2].spans[0]}</span>
-      <span class="client-hidden">${cardetails[2].spansdsk[0]}</span>
-      <span><i class="fa fa-circle fa-xs"></i></span>
-      <span class="role">${cardetails[2].spans[1]}</span>
-      <span><i class="fa fa-circle"></i></span>
-      <span class="year">${cardetails[2].spans[2]}</span>
-    </div>
-    <p class="card-primary-text">${cardetails[2].textcontent}</p>
-  </div>
-  <ul class="tags">
-    <li class="tag">${cardetails[2].technologies[0]}</li>
-    <li class="tag">${cardetails[2].technologies[1]}</li>
-    <li class="tag-hidden">${cardetails[2].technologiesdsk[2]}</li>
-    <li class="tag">${cardetails[2].technologies[2]}</li>
-  </ul>
-  <div class="ref">
-    <button class="popup-btn" id="3">${cardetails[2].btntxt}</button>
-  </div>
-</div>`;
-
-cardWorks3.className = 'works-4';
-const workCard3 = document.createElement('article');
-const workCardDiv3 = document.createElement('div');
-workCardDiv3.className = 'card-works';
-cardWorks3.appendChild(workCard3);
-workCard3.className = 'card-works';
-workCard3.innerHTML = `<img src=${cardetails[3].img} alt="snapshootimg" class="snapshoot-img">
-<div class="adjust">
-  <div class="leftblock">
-    <h2 class="card-works-title-1 popup-title">${cardetails[3].titledsk}</h2>
-    <h2 class="card-works-title-hidden-1">${cardetails[3].titledsk}</h2>
-    <div class="ref">
-      <span class="client-1">${cardetails[3].spans[0]}</span>
-      <span class="client-hidden-2">${cardetails[3].spansdsk[0]}</span>
-      <span><i class="fa fa-circle fa-xs"></i></span>
-      <span class="role">${cardetails[3].spans[1]}</span>
-      <span><i class="fa fa-circle"></i></span>
-      <span class="year">${cardetails[3].spans[1]}</span>
-    </div>
-    <p class="card-primary-text">${cardetails[3].textcontent}</p>
-  </div>
-  <ul class="tags">
-    <li class="tag">${cardetails[3].technologies[0]}</li>
-    <li class="tag">${cardetails[3].technologies[1]}</li>
-    <li class="tag-hidden">${cardetails[3].technologiesdsk[2]}</li>
-    <li class="tag">${cardetails[3].technologies[2]}</li>
-  </ul>
-  <div class="ref">
-    <button class="popup-btn" id="4">${cardetails[2].btntxt}</button>
-  </div>
-</div>
-<img src=${cardetails[3].imagedsk} alt="snapshoot-img" class="snapshoot-img-dsk-2">`;
-
-cardWorks.appendChild(workCard);
-
-const buttons = document.querySelectorAll('.popup-btn');
 const modal = document.getElementById('myModal');
-const span = document.getElementsByClassName('close-popup')[0];
 
-const spanone = () => {
-  modal.style.display = 'none';
-};
+function modalClick(i) {
+  modal.innerHTML =`
+  <div class="modal">
+        <!-- Modal content -->
+        <div class="frame-desktop">
+          <h2 class="frame-mobile-h2">${cardetails[i].title}</h2>
+          <span id="close-popup" class="close-popup">&times;</span>
+        </div>
+        <div class="spans-popup">
+          <span class="client">Canopy</span>
+          <span><i class="fa fa-circle fa-xs"></i></span>
+          <span class="role"> Back End Dev</span>
+          <span><i class="fa fa-circle"></i></span>
+          <span class="year">2015</span>
+        </div>
+        <img src=${cardetails[i].img} alt="Snapshoot-Portfolio" class="img-popup" id="popupimage">
+        <img src=${cardetails[i].img} alt="mobile-popup" class="img-popup-mobile" id="popupimage">
+        <div class="modal-container"> 
+          <div class="information-popup">
+            <p class="paragraph-popup">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              <br>
+              <br>
+              <br>
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.
+            </p>
+            <p class="paragraph-popup-mobile">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
+            </p>
+          </div>
+          <div class="tags-button">
+            <ul class="tags">
+              <li class="tag">${cardetails[i].technologies[0]}</li>
+              <li class="tag">${cardetails[i].technologies[1]}</li>
+              <li class="tag">${cardetails[i].technologies[0]}</li>
+            </ul>
+            <ul class="tags-hidden">
+              <li class="tag">Ruby</li>
+              <li class="tag">Bootstrap</li>
+            </ul>
+            <hr>
+            <div class="btn-block">
+              <a href="https://joaquin123455.github.io/microverse-portfolio/" target="_blank" rel="noopener" class="btn-a">
+              <button class="btn-popup-left" type="button"> See Live <img class="img-btn"
+                  src="./img/live-popup.svg" alt="Button of project "> </button></a>
+              <a href="https://github.com/Joaquin123455" target="_blank" rel="noopener" class="btn-a">
+              <button class="btn-popup-right" type="button">See Source <img class="img-btn"
+                  src="./img/github-popup.svg" alt="Button of repository"> </button></a>
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
 
-const close = (event) => {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-};
-
-window.addEventListener('click', close);
-span.addEventListener('click', spanone);
-
-// add Listener Event to each see-project button
-for (let i = 0; i <= buttons.length; i += 1) {
-  buttons[i].addEventListener('click', () => {
-    modal.style.display = 'block';
-    const title = document.querySelectorAll('.popup-title');
-    document.querySelector('.frame-mobile-h2').innerHTML = title[i].innerHTML;
-    const imagedsk = document.querySelectorAll('popupimg');
-    document.querySelector('.popup-image').innerHTML = imagedsk[i].innerHTML;
-    const spansdsk = document.querySelectorAll('popupspan');
-    document.querySelector('.client').innerHTML = spansdsk[i].innerHTML;
+  const closeButton = document.getElementById('close-popup');
+  closeButton.addEventListener('click', () => {
+    modal.classList.add('display-none');
   });
 }
+
+
+const buttons = document.querySelectorAll('.popup-btn');
+
+buttons.forEach((button, i) => {
+  button.addEventListener('click', () => {
+    modal.classList.remove('display-none');
+    modalClick(i);
+  });
+});
